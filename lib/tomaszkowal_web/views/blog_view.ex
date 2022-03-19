ce1100697886e7c56212cb387905b42c06890c01
Tomasz Kowal <t.kowal@bluecode.com>
2022-03-19 14:08:44 +0100

    Add tag colors

--- lib/tomaszkowal_web/views/blog_view.ex
+++ lib/tomaszkowal_web/views/blog_view.ex
@@ -1,3 +1,13 @@
 defmodule TomaszkowalWeb.BlogView do
   use TomaszkowalWeb, :view
+
+  def color_tags(tags) do
+    (for tag <- tags, do: color_tag(tag))
+    |> Enum.join()
+    |> raw()
+  end
+
+  # I like Notion colors https://optemization.com/notion-color-guide
+  defp color_tag("nix"), do: "<span style=\"background: #DDEBF1\">nix</span>"
+  defp color_tag(other), do: "<span style=\"background: #EBECED\">#{other}</span>"
 end
