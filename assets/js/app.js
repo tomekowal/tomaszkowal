dca3fef87b846a296b2daa6c92935f31746d990c
Tomasz Kowal <t.kowal@bluecode.com>
2023-03-01 21:31:12 +0100

    Add .tool-versions and mermaid.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,9 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+import mermaid from "mermaid";
+mermaid.initialize({ startOnLoad: true });
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
